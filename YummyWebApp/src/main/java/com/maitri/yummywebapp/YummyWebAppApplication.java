a4503c97972ce1e16903633efda15b8ce37ee5fb
maitri.trivedi <trivedimaitri1@gmail.com>
2024-11-01 20:46:39 +0530

    First Commit : Started Maven Spring Project

---
+++ YummyWebApp/src/main/java/com/maitri/yummywebapp/YummyWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.maitri.yummywebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YummyWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YummyWebAppApplication.class, args);
+	}
+
+}
---
+++ YummyWebApp/src/test/java/com/maitri/yummywebapp/YummyWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maitri.yummywebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YummyWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
